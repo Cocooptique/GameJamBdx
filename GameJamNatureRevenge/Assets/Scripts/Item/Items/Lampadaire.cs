428ca257732365e9fde5bb679b993ec4a1ceec69
Cocooptique <corentin.mercier@insitutoptique>
2015-11-14 08:22:51 +0100

    Personnage de test

---
+++ GameJamNatureRevenge/Assets/Scripts/Item/Items/Lampadaire.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Lampadaire : MonoBehaviour {
+    public float radius = 4.0f;
+
+    
+    void Update()
+    {
+        GetComponent<CapsuleCollider>().radius = radius;
+        GetComponent<Light>().range = radius;
+    }
+
+}
